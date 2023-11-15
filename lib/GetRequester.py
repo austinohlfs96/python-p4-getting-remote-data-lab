1ae0ed90eca7d8d1bdbe8f462e4505f2726c8867
Austin Ohlfs <ohlfsam96@gmail.com>
2023-11-15 09:15:34 -0700

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        # data_list = []
+        # data = json.loads(self.get_response_body())
+        # for item in data:
+        #     data_list.append(item("employee"))
+            
+        # return data_list
+        response_body = self.get_response_body()
+        json_data = json.loads(response_body)
+        return json_data
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
